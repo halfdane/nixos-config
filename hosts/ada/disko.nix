f2bde6800fa477e49afa3f9f72e1d1de2862c224
halfdane <halfdane@gmx.net>
2026-02-10 08:40:37 +0100

    [ada] add subvolumes, snapshots, swapfile

--- hosts/ada/disko.nix
+++ hosts/ada/disko.nix
@@ -29,16 +29,38 @@
             content = {
               type = "luks";
               name = "luks-root";
+              settings.allowDiscards = true;
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ];
-                mountOptions = [ "noatime" "compress=zstd" "space_cache=v2" ];
                 subvolumes = {
                   "@" = {
                     mountpoint = "/";
+                    mountOptions = [ "noatime" "compress=zstd:1" "space_cache=v2" ];
                   };
                   "@home" = {
                     mountpoint = "/home";
+                    mountOptions = [ "noatime" "compress=zstd:1" "space_cache=v2" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "noatime" "compress=zstd:1" "space_cache=v2" ];
+                  };
+                  "@varlog" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [ "noatime" "compress=zstd:1" "space_cache=v2" ];
+                  };
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "noatime" "compress=zstd:1" "space_cache=v2" ];
+                  };
+                  "@swapfile" = {
+                    mountpoint = "/swapfile";
+                    mountOptions = [ "noatime" "nodatacow" "nodatasum" "space_cache=v2" ];
+                  };
+                  "@data" = {
+                    mountpoint = "/data";
+                    mountOptions = [ "noatime" "compress=zstd:1" "space_cache=v2" ];
                   };
                 };
               };
@@ -48,4 +70,58 @@
       };
     };
   };
+
+  # Swapfile (8GB example)
+  swapDevices = [{
+    device = "/swapfile/swapfile";
+    size = 8192;
+    randomEncryption.enable = true;
+  }];
+
+  # Swapfile setup (disable CoW, create file)
+  systemd.services.initrd-setup-swapfile = {
+    wantedBy = [ "initrd-setup.service" ];
+    before = [ "initrd-setup.service" ];
+    script = ''
+      chattr +C /swapfile
+      truncate -s 8G /swapfile/swapfile
+      mkswap /swapfile/swapfile
+    '';
+  };
+
+  # Hourly Snapper on /home (prunes aggressively)
+  services.snapper.configs.home = {
+    SUBVOLUME = "/home";
+    FSTYPE = "btrfs";
+    ALLOW_USERS = [ "yourusername" ];  # Replace
+    TIMELINE_CREATE = true;
+    TIMELINE_CLEANUP = true;
+    TIMELINE_MIN_AGE = "1800";
+    TIMELINE_LIMIT_HOURLY = "5";
+    TIMELINE_LIMIT_DAILY = "7";
+    TIMELINE_LIMIT_WEEKLY = "4";
+    TIMELINE_LIMIT_MONTHLY = "3";
+  };
+
+  # /home quota (50GB)
+  # systemd.services.btrfs-home-quota = {
+  #   description = "Set BTRFS quota for /home";
+  #   after = [ "local-fs.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig.Type = "oneshot";
+  #   script = ''
+  #     btrfs quota enable /
+  #     HOME_QGROUP=$(btrfs qgroup show /home | grep -o '0/[0-9]*' | head -1)
+  #     btrfs qgroup create $HOME_QGROUP /
+  #     btrfs qgroup limit 50G $HOME_QGROUP /
+  #   '';
+  # };
+
+  # Optional: Impermanence (@persist for state)
+  # Uncomment + add environment.persistence."/persist" for your files
+  # fileSystems."/persist" needs UUID or label reference if needed
+  # environment.persistence."/persist" = {
+  #   directories = [ "/var/lib" ];
+  #   files = [ "/etc/machine-id" ];
+  # };
 }
