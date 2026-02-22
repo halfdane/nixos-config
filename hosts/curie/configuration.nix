2d23ec344828c7e43c22a6a8f1ac5d2e9c676429
halfdane <halfdane@gmx.net>
2026-02-22 13:27:59 +0100

    enable snapshots

--- hosts/curie/configuration.nix
+++ hosts/curie/configuration.nix
@@ -152,4 +152,18 @@ in
     gcc.cc.lib
     zlib
   ];
+
+      # Hourly Snapper on /home (prunes aggressively)
+  services.snapper.configs.home = {
+    SUBVOLUME = "/home";
+    FSTYPE = "btrfs";
+    ALLOW_USERS = [ "yourusername" ];  # Replace
+    TIMELINE_CREATE = true;
+    TIMELINE_CLEANUP = true;
+    TIMELINE_MIN_AGE = "1800";
+    TIMELINE_LIMIT_HOURLY = "5";
+    TIMELINE_LIMIT_DAILY = "7";
+    TIMELINE_LIMIT_WEEKLY = "4";
+    TIMELINE_LIMIT_MONTHLY = "3";
+  };
 }
