a587fe1fb9e2205f1cea628f746612b8b8b2467a
halfdane <halfdane@gmx.net>
2026-02-22 15:13:37 +0100

    firefox: add tab containers

--- home/firefox.nix
+++ home/firefox.nix
@@ -6,8 +6,10 @@
         Install = [
           "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi"
           "https://addons.mozilla.org/firefox/downloads/latest/ghostery/addon-9609-latest.xpi"
+          "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/addon-654046-latest.xpi"
         ];
       };
     };
   };
 }
+
