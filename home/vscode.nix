e9858d3ead4639483b35a405185d753ed77221e7
halfdane <halfdane@gmx.net>
2026-03-12 19:05:53 +0100

    feat: update vscode settings configuration to conditionally link settings file

--- home/vscode.nix
+++ home/vscode.nix
@@ -14,11 +14,14 @@
     ];
   };
 
-  home.file."${config.xdg.configHome}/Code/User/settings.json" = {
-    source = lib.mkForce  (
-      config.lib.file.mkOutOfStoreSymlink "/home/user/nixos-config/home/vscode_settings.json"
-    );
-    force = true;
-  };
+  ## whenever vscode changes anything, the settings are actually stored in ./vscode_settings.json
+  home.file."${config.xdg.configHome}/Code/User/settings.json" = 
+    lib.mkIf config.programs.vscode.enable
+    {
+      source = lib.mkForce  (
+        config.lib.file.mkOutOfStoreSymlink /home/user/nixos-config/home/vscode_settings.json
+      );
+      force = true;
+    };
 }
 
