a7d50a32a5df3a56e42b3d052d06ab5edd34c248
halfdane <halfdane@gmx.net>
2026-03-12 13:50:01 +0100

    feat: enhance Beets configuration with additional plugins and settings

--- hosts/ada/home.nix
+++ hosts/ada/home.nix
@@ -9,7 +9,37 @@
     settings = {
       directory = "/data/Music";
       library = "/data/Music/library.db";
-      plugins = [ "web" "spotify" ];
+      plugins = [ "info" "scrub" "missing" "duplicates" "ftintitle" "fetchart" "musicbrainz" "spotify" "lyrics" "lastgenre" ];
+      import = {
+        move = true;
+        duplicate_action = "merge";
+        incremental = true;
+        incremental_skip_later = false;
+      };
+      paths = {
+        default = "$album_artist_no_feat/$year-$album/$track-$title";
+      };
+      asciify_paths = true;
+
+      musicbrainz = {
+        genres = false;
+        data_source_mismatch_penalty = 0.1;  # Lower penalty = preferred
+      };
+      spotify.data_source_mismatch_penalty = 0.9;
+      lastgenre = {
+        canonical = true;
+        whitelist = true;
+        count = 3;
+      };
+      lyrics = {
+        auto = true;
+        dist_thresh = 0.11;
+        fallback = null;
+        force = false;
+        print = false;
+        sources = [ "lrclib" ];
+        synced = false;
+      };
     };
   };
 }
