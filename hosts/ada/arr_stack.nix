731805f15ca8bf2cd82599af3965bcd25bb7c2cb
halfdane <halfdane@gmx.net>
2026-03-14 15:14:28 +0100

    feat: remove qbittorrent service configuration from arr_stack.nix

--- hosts/ada/arr_stack.nix
+++ hosts/ada/arr_stack.nix
@@ -3,17 +3,6 @@ let domain = "micasaestu.casa"; in {
   services.prowlarr.enable = true;
   services.sonarr = { enable = true; group = "media"; };
   services.radarr = { enable = true; group = "media"; };
-  services.qbittorrent = {
-    enable = true;
-    group = "media";
-    serverConfig = {
-      LegalNotice.Accepted = true;
-      Preferences.WebUI = {
-        LocalHostAuth = true;
-        extraArgs = "--confirm-legal-notice";
-      };
-    };
-  };
 
   services.nginx.virtualHosts."prowlarr.${domain}" = {
     useACMEHost = domain;
@@ -36,14 +25,6 @@ let domain = "micasaestu.casa"; in {
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://127.0.0.1:7878";
-      proxyWebsockets = true;
-    };
-  };
-  services.nginx.virtualHosts."qbittorrent.${domain}" = {
-    useACMEHost = domain;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:8080";
       proxyWebsockets = true;
     };
   };
