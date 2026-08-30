b5931fec5d36ef5cd3f7b2bd255188aa84c11c30
halfdane <halfdane@gmx.net>
2026-08-30 15:50:35 +0200

    fix(work-system): update imports for VPN and certificates configuration

--- hosts/curie/work-system.nix
+++ hosts/curie/work-system.nix
@@ -3,7 +3,12 @@
   # All work system-level config lives in the minerva_owl-setup repo, fetched
   # as the `minerva_owl_setup` flake input (see flake.nix).
   # VPN (globalprotect-openconnect) is provided by minerva_owl_setup.
-  imports = [ inputs.minerva_owl_setup.nixosModules.workSystem ];
+  imports = [
+    inputs.minerva_owl_setup.nixosModules.certificates
+    inputs.minerva_owl_setup.nixosModules.vpn
+  ];
+
+  services.minerva-vpn.enable = true;
 
   # dash0 CLI etc. (see minerva_owl-setup/home/module.nix); useGlobalPkgs = true
   # means overlays must be applied here at the system level, not from within
